5512ea985371d76b774c7578bd91d40980bd813c
gogoprog <gogoprog@gmail.com>
2012-09-27 12:28:31 +0200

    Updated util class

--- www/classes/util.php
+++ www/classes/util.php
@@ -4,9 +4,9 @@ class Util
 {
     public static function decodeJson()
     {
-        if(isset($_GET["json"]))
+        if(isset($_REQUEST["json"]))
         {
-            $json = $_GET["json"];
+            $json = $_REQUEST["json"];
             $json = json_decode($json, true)[0];
             return $json;
         }
